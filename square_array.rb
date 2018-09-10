8fcf93469920102f16e4242b0c9484613b62ea4e
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-09-10 15:16:02 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   sqrt = Array.new
   array.each do |squared|
-    add_sqrt = Math.sqrt(array[0])
-    sqrt.unshift(add_sqrt)
+    Math.sqrt(array[0])
     array[0] += 1
   end
 end
